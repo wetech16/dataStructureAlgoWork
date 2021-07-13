ba4b107d72168a824892f46eccc5fd6f473f7edb
wetech16 <67137441+wetech16@users.noreply.github.com>
2021-07-13 15:38:10 -0700

    update

--- rotatedArray.js
+++ rotatedArray.js
@@ -7,10 +7,10 @@
       let mid = Math.floor(low / 2 + high / 2);
       let prev = (mid + n - 1) % n;
       let next = (mid + 1) % n;
-      if (arr[low] < arr[high]) return low;
+      if (arr[low] < arr[high]) return low; //it's sorted
       if (arr[mid] <= arr[next] && arr[mid] <= arr[prev]) return mid;
-      if (arr[mid] <= arr[high]) high = mid - 1;
-      if (arr[mid] >= arr[low]) low = mid + 1;
+      if (arr[mid] <= arr[high]) high = mid - 1; // right part is sorted
+      if (arr[mid] >= arr[low]) low = mid + 1; // left part is sorted
     }
   }
   let arr = [12, 14, 15, 4];
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function main(n) {
   function isPowerTwo(n) {
     let i = 0;
     let total = 0;
-    if (n === 0) return false;
+    if (n == 0) return false;
     while (total < n) {
       total = Math.pow(2, i);
       i++;

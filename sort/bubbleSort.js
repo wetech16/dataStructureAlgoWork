061eded167d6e91560b3281e3419b407cdec4375
wetech16 <67137441+wetech16@users.noreply.github.com>
2022-06-02 12:28:33 -0700

    Update bubbleSort.js

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -46,3 +46,18 @@ console.log("sorted", a);
 //     arr[b] = temp;
 //   }
 // }
+
+function sortArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(j, j + 1);
+      }
+    }
+  }
+  function swap(a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+}
